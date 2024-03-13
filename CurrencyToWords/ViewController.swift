618f1ff2f700bae77bd203d3a1ba7d62ccb77c7f
bathalasupraja <bathalasupraja2@gmail.com>
2024-03-13 20:52:50 +0530

    Added pickerView into text field
    * added UIPickerView into viewController
    * added textField  and create languages model

--- CurrencyToWords/ViewController.swift
+++ CurrencyToWords/ViewController.swift
@@ -8,31 +8,73 @@
 import UIKit
 import AVFoundation
 
+struct LanguageModel {
+    var name: String
+    var code: String
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var currencyTextField: UITextField!
     @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var languageTextField: UITextField!
     @IBOutlet weak var speakButton: UIButton!
     
     var pickerView = UIPickerView()
+   // var languagesPickerView: UIPickerView!
     
     let currencies = ["rupee", "doller", "dinar"]
+    var languages = [LanguageModel]()
+    
     var speechSynthesizer: AVSpeechSynthesizer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         numberTextField.delegate = self
         currencyTextField.delegate = self
+        languageTextField.delegate = self
         
         numberTextField.tag = 1
         currencyTextField.tag = 2
+        languageTextField.tag = 3
         
         pickerView.delegate = self
         pickerView.dataSource = self
+       // languagesPickerView.delegate = self
+        //languagesPickerView.dataSource = self
         currencyTextField.inputView = pickerView
+        languageTextField.inputView = pickerView
         
         speakButton.layer.cornerRadius = 4
         speakButton.layer.masksToBounds = true
+    }
+    
+    func createLanguages() {
+        languages.append(LanguageModel(name: "Afrikaans", code: "af"))
+        languages.append(LanguageModel(name: "Basque", code: "eu"))
+        languages.append(LanguageModel(name: "Chinese (Hong Kong)", code: "zh-hk"))
+        languages.append(LanguageModel(name: "Dutch (Belgium)", code: "nl-be"))
+        languages.append(LanguageModel(name: "English", code: "en"))
+        languages.append(LanguageModel(name: "French (Belgium)", code: "fr-be"))
+        languages.append(LanguageModel(name: "German (Austria)", code: "de-at"))
+        languages.append(LanguageModel(name: "Hindi", code: "hi"))
+        languages.append(LanguageModel(name: "Irish", code: "ga"))
+        languages.append(LanguageModel(name: "Japanese", code: "ja"))
+        languages.append(LanguageModel(name: "Korean", code: "ko"))
+        languages.append(LanguageModel(name: "Latvian", code: "lv"))
+        languages.append(LanguageModel(name: "Malayalam", code: "ml"))
+        languages.append(LanguageModel(name: "Norwegian", code: "no"))
+        languages.append(LanguageModel(name: "Punjabi", code: "pa"))
+        languages.append(LanguageModel(name: "Russian", code: "ru"))
+        languages.append(LanguageModel(name: "Spanish (Mexico)", code: "es-mx"))
+        languages.append(LanguageModel(name: "Thai", code: "th"))
+        languages.append(LanguageModel(name: "Urdu", code: "ur"))
+        languages.append(LanguageModel(name: "Venda", code: "ve"))
+        languages.append(LanguageModel(name: "Welsh", code: "cy"))
+        languages.append(LanguageModel(name: "Xhosa", code: "xh"))
+        languages.append(LanguageModel(name: "Yiddish", code: "ji"))
+        languages.append(LanguageModel(name: "Zulu", code: "zu"))
+        
     }
     
     /// Trigger when user click oin the OK button
@@ -40,7 +82,7 @@ class ViewController: UIViewController {
         numberTextField.resignFirstResponder()
         currencyTextField.resignFirstResponder()
         if let numberText = self.numberTextField.text,
-           let number = Int(numberText), let currency = currencyTextField.text, currency.count > 0, number > 0 {
+           let number = Int(numberText), let currency = currencyTextField.text, let languages = languageTextField.text, currency.count > 0, number > 0, languages > languages {
             let formatter = NumberFormatter()
             formatter.numberStyle = .spellOut
             if let string = formatter.string(from: number as NSNumber) {
@@ -73,6 +115,10 @@ class ViewController: UIViewController {
 
 // MARK: - UITextFieldDelegate
 extension ViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        pickerView.tag = textField.tag
+        return true
+    }
     func textFieldDidBeginEditing(_ textField: UITextField) {
         if textField.tag == 2 { /// 2 - currency
             textField.text = currencies.first
@@ -87,16 +133,24 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        currencies.count
+        if pickerView.tag == 2 {
+            return currencies.count
+        } else {
+            return languages.count
+        }
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
         let currency = currencies[row]
+        currencyTextField.text = currency
         return currency
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let currency = currencies[row]
-        currencyTextField.text = currency
+      //  if pickerView.tag == 2 {
+            let currency = currencies[row]
+            currencyTextField.text = currency
+        }
     }
-}
+//}
+
